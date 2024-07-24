77fb8972826ea3a7b2ffb95d2dc81b9e8ef003f4
Pascal Owilly <owillypascal@gmail.com>
2024-07-24 13:09:25 +0300

    update

--- scm/settings.py
+++ scm/settings.py
@@ -149,10 +149,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  # Use the appropriate port for your SMTP server
 EMAIL_USE_TLS = True  # Set to False if your server doesn't use TLS
-# EMAIL_HOST_USER = 'intellima.tech@gmail.com'  # Your email address
-# EMAIL_HOST_PASSWORD = 'txqerssmxheiyruz'
-EMAIL_HOST_USER = 'pascalouma54@gmail.com'  # Your email address
-EMAIL_HOST_PASSWORD = 'jcfgolmahddantnv'
+EMAIL_HOST_USER = 'intellima.tech@gmail.com'  # Your email address
+EMAIL_HOST_PASSWORD = 'txqerssmxheiyruz'
+# EMAIL_HOST_USER = 'pascalouma54@gmail.com'  
+# EMAIL_HOST_PASSWORD = 'jcfgolmahddantnv'
+#EMAIL_HOST_USER = 'pascal.owilly@student.moringaschool.com'
+#EMAIL_HOST_PASSWORD = 'ymxllqbalildvjri'
 EMAIL_USE_SSL = False
 
 BASE_URL = '127.0.0.1:8000'
