edb98c9308307474146172d5c9af21c628238f8a
Pascal Owilly <owillypascal@gmail.com>
2025-01-27 14:34:12 +0300

    up

--- scm/settings.py
+++ scm/settings.py
@@ -293,6 +293,10 @@ DATABASES = {
 }
 
 STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+        'LOCATION': os.path.join(BASE_DIR, 'media'),
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage", 
     },
